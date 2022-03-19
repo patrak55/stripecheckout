cfa84fbe0a3fea483f0df0cb8421ad2ed3819d19
patrak55 <pchemoul@gmail.com>
2022-03-18 18:45:47 -0700

    stripe prod try to fix

--- next.config.js
+++ next.config.js
@@ -3,10 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = 
+
 
 module.exports = {
+  mode: 'universal',
+  env: {
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+      NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
 
+
+  },
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com'],
